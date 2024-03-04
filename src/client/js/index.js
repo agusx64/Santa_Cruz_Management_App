2592c765ff8e67523ed7958ef137c87b90b7532b
Agus Mora <agusm1253@gmail.com>
2024-03-03 21:35:39 -0600

    Santa_Cruz_Management_App_v1.0

---
+++ src/client/js/index.js
@@ -0,0 +1,5 @@
+//Cada archivo CSS que se valla a utilizar se debe de declarar aqui, 
+//para que el servidor pueda leer los estilos :)
+
+//CSS del index principal
+require('../css/style.css');
---
+++ src/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackDevConfig = require('../webpack.config');
+const webpackConfig = require('../webpack.config');
+
+var app = express();
+app.set('port',(process.env.PORT || 3000));
+app.use('/static', express.static('dist'));
+app.use(webpackDevMiddleware(webpack(webpackConfig)));
+
+app.get('/', function(req, res, next){
+    res.send('Loading page_tree');
+});
+
+app.listen(app.get('port'),() => {
+    console.log('Servidor activo y corriendo');
+})
