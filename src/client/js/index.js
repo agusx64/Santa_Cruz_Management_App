4b61e1dc891d8c62a4dffc5ecf1baaf00f9a706f
Agus Mora <agusm1253@gmail.com>
2024-03-04 23:18:17 -0600

    Prueba de pantalla completa

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -3,3 +3,20 @@
 
 //CSS del index principal
 require('../css/style.css');
+
+// Función para activar la pantalla completa
+function activarPantallaCompleta() {
+    var elemento = document.documentElement;
+    if (elemento.requestFullscreen) {
+        elemento.requestFullscreen();
+    } else if (elemento.mozRequestFullScreen) { /* Firefox */
+        elemento.mozRequestFullScreen();
+    } else if (elemento.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        elemento.webkitRequestFullscreen();
+    } else if (elemento.msRequestFullscreen) { /* IE/Edge */
+        elemento.msRequestFullscreen();
+    }
+}
+
+// Asociar la función con el evento clic del botón
+document.getElementById("botonPantallaCompleta").addEventListener("click", activarPantallaCompleta);
