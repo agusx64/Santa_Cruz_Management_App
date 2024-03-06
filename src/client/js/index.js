1e6f1e1ef44808babdcc525b097a581e587ef74b
Agus Mora <agusm1253@gmail.com>
2024-03-05 20:09:22 -0600

    Agregado de docs html y css

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -3,3 +3,25 @@
 
 //CSS del index principal
 require('../css/style.css');
+
+//HTML group_1
+require('../css/css_group_1/administrador.css');
+require('../css/css_group_1/datos_paciente.css');
+require('../css/css_group_1/datos_trabajador.css');
+require('../css/css_group_1/inicio.css');
+require('../css/css_group_1/listado_medicamentos');
+
+//HTML group_2
+require('../css/css_group_2/calendario.css');
+require('../css/css_group_2/farmacia.css');
+require('../css/css_group_2/homepage.css');
+require('../css/css_group_2/login.css');
+require('../css/css_group_2/settings.css');
+
+//HTML group_3
+require('../css/css_group_3/agregar_correo.css');
+require('../css/css_group_3/detalles_instrumentos.css');
+require('../css/css_group_3/detalles_medicamentos.css');
+require('../css/css_group_3/informacion_infantes');
+require('../css/css_group_3/listado_infantes.css');
+require('../css/css_group_3/listado_instrumentos');
