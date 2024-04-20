12239eac6aea62bcf53a61885715182d75182b8f
Jess01234 <162223551+Jess01234@users.noreply.github.com>
2024-04-20 12:04:42 -0600

    CSS

--- public/login/login.js
+++ public/login/login.js
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
         var input_password = document.getElementById('input_password').value;
 
         if (input_email === "Agus" && input_password === "1234") {
-            window.location.href = "/src/client/file_group_1/administrador/administrador.html";
+            window.location.href = "/administrador/administrador.html";
         }
         else if (input_email === "Jess" && input_password === "1234"){
-            window.location.href = "/src/client/file_group_2/homepage/homepage.html";
+            window.location.href = "/homepage/homepage.html";
         }
     });
 });
