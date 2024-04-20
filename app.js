623b0a07019e73a8dfab1d92f64e0f6d22927c18
Jess01234 <162223551+Jess01234@users.noreply.github.com>
2024-04-20 11:07:13 -0600

    EJS

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-    res.send("<h1> Hi");
-});
-
 app.use(express.static("public"));
 
 app.listen(3000, function(){
     console.log("servidor activo")
-})
+});
